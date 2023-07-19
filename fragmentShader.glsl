7536e44a0e828162cb6c009b91a51d6833a17368
osorokin <sorokin.oleg.edu@gmail.com>
2023-07-19 16:38:40 +0300

    Initial commit

---
+++ fragmentShader.glsl
@@ -0,0 +1,34 @@
+#version 330 core
+
+uniform vec2 screenSize;
+uniform vec2 offset;
+uniform float zoom;
+
+out vec4 returnColor;
+
+vec2 multiplyComplex(vec2 a, vec2 b) {
+    return vec2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);
+}
+
+void main()
+{
+    vec2 uv = (gl_FragCoord.xy / screenSize - 0.5) * zoom + 0.5 + offset / screenSize;
+
+    int jumps = 500;
+
+    vec3 color = vec3(1.0, 0.0, 0.0);
+
+    vec2 z = vec2(0.0, 0.0);
+    vec2 c = uv.xy;
+
+    for (int i = 0; i <= jumps; i++) {
+        z = multiplyComplex(z, z) + c;
+        if (length(z) >= 10.0 / zoom) {
+            float n = float(i) / float(jumps);
+            color = vec3(n, 0.0, 0.0);
+            break;
+        }
+    }
+
+    returnColor = vec4(color, 1.0);
+}
---
+++ main.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+std::string readFile(const std::string& filePath)
+{
+    std::ifstream fileStream(filePath);
+    std::stringstream stringStream;
+    stringStream << fileStream.rdbuf();
+    return stringStream.str();
+}
+
+GLuint compileShader(GLenum shaderType, const std::string& shaderSource)
+{
+    GLuint shader = glCreateShader(shaderType);
+    const char *source = shaderSource.c_str();
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "Shader compilation error: " << infoLog << std::endl;
+    }
+
+    return shader;
+}
+
+GLuint createShaderProgram(const std::string& vertexShaderSource, const std::string& fragmentShaderSource)
+{
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Shader program linking error: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+float quadVertices[] = {
+        -1.0f,  1.0f,
+        -1.0f, -1.0f,
+        1.0f, -1.0f,
+
+        -1.0f,  1.0f,
+        1.0f, -1.0f,
+        1.0f,  1.0f
+};
+
+double offsetX = 0.0f;
+double offsetY = 0.0f;
+double zoom = 1.0f;
+
+void cursorPositionCallback(GLFWwindow* window, double xPosition, double yPosition)
+{
+    static double lastPositionX = xPosition;
+    static double lastPositionY = yPosition;
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        offsetX += (xPosition - lastPositionX) * zoom;
+        offsetY += (yPosition - lastPositionY) * zoom;
+    }
+
+    lastPositionX = xPosition;
+    lastPositionY = yPosition;
+}
+
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+    zoom += yOffset * 0.05f * zoom;
+}
+
+int main()
+{
+    // Initialize GLFW and create a window
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    int aspectW = 16;
+    int aspectH = 9;
+    int aspectN = 200;
+    GLFWwindow* window = glfwCreateWindow(aspectW * aspectN, aspectH * aspectN, "Fractals", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Load and compile shaders
+
+    std::string vertexShaderSource = readFile("vertexShader.glsl");
+    std::string fragmentShaderSource = readFile("fragmentShader.glsl");
+
+    GLuint shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+    GLuint quadVBO;
+    glGenBuffers(1, &quadVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+
+    GLuint quadVAO;
+    glGenVertexArrays(1, &quadVAO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*) nullptr);
+    glEnableVertexAttribArray(0);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glfwSetScrollCallback(window, scrollCallback);
+    glfwSetCursorPosCallback(window, cursorPositionCallback);
+
+    // Rendering loop
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear the screen
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        int screenWidth, screenHeight;
+        glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+        GLint screenSizeLocation = glGetUniformLocation(shaderProgram, "screenSize");
+        glUseProgram(shaderProgram);
+        glUniform2f(screenSizeLocation, (float) screenWidth, (float) screenHeight);
+
+        GLint offsetLocation = glGetUniformLocation(shaderProgram, "offset");
+        glUseProgram(shaderProgram);
+        glUniform2f(offsetLocation, (float) offsetX, (float) offsetY);
+
+        GLint zoomLocation = glGetUniformLocation(shaderProgram, "zoom");
+        glUseProgram(shaderProgram);
+        glUniform1f(zoomLocation, (float) zoom);
+
+        // Render the screen
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(quadVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Swap buffers and poll events
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+
+    return 0;
+}
